ad8f64d84f990036de511a592c83589f338ff4eb
Henrique <henrimrc@hotmail.com>
2025-06-08 19:31:23 +0100

    Implemented GetByName

--- ScreenSound/database/ArtistDAL.cs
+++ ScreenSound/database/ArtistDAL.cs
@@ -2,6 +2,7 @@
 using screensound.models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace screensound.database
 {
@@ -15,6 +16,26 @@ namespace screensound.database
         }
 
         public List<Artist> GetList() => _context.Artists.ToList();
+
+        public List<Artist> GetByName(string name) => GetByNameAsync(name).Result;
+        public async Task<List<Artist>> GetByNameAsync(string name)
+        {
+            List<Artist> output = new();
+            await foreach (Artist artist in _context.Artists)
+                if (artist.Name == name)
+                    output.Add(artist);
+            return output;
+        }
+
+        public Artist? GetFirstByName(string name) => GetFirstByNameAsync(name).Result;
+        public async Task<Artist?> GetFirstByNameAsync(string name)
+        {
+            await foreach (Artist artist in _context.Artists)
+                if (artist.Name == name)
+                    return artist;
+
+            return null;
+        }
 
         public EntityEntry<Artist> Add(Artist artist)
         {
