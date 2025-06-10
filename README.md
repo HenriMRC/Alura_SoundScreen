faf9177f5a618776b5f4f436ac3ba7cca5bdf9a0
Henrique <henrimrc@hotmail.com>
2025-06-10 09:47:36 +0100

    Added README.md and vscode workspace. Created first migration.

---
+++ README.md
@@ -0,0 +1,11 @@
+# **Course**: Sound Screen Project
+
+Project of course from **Alura**
+
+Course: [.NET: data persistance with Entity Framework Core](https://cursos.alura.com.br/course/dot-net-persistindo-dados-entity-framework-core)
+
+Using `Microsoft.EntityFrameworkCore.* Version="7.0.20"` and `Microsoft.Data.SqlClient Version="5.2.3"` for combility with `net6`
+
+Notes:
+- To use a migration go to `Tools > NuGet Package Manager > Package ManagerConsole`
+- Command `Add-Migration <name>` captures model changes since the last migration and generating migration files from those changes. [*Source*](https://www.learnentityframeworkcore.com/migrations/add-migration)
---
+++ ScreenSound/Migrations/20250610084231_InitialState.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace screensound.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialState : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfileImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Musics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Musics", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Artists");
+
+            migrationBuilder.DropTable(
+                name: "Musics");
+        }
+    }
+}
