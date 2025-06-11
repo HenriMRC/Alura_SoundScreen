9134b194c45717649ea1e081d8cb4447e1dca9e7
Henrique <henrimrc@hotmail.com>
2025-06-11 22:26:00 +0100

    Created migration PopulateMusics

---
+++ ScreenSound/Migrations/20250611212208_PopulateMusics.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace screensound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateMusics : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder build)
+        {
+            const string TABLE = "Musics";
+            string[] columns = new string[] { "Name", "YearOfRelease" };
+;
+            build.InsertData(TABLE, columns, new object[] { "Oceano", 1989 });
+            build.InsertData(TABLE, columns, new object[] { "Flor de Lis", 1976 });
+            build.InsertData(TABLE, columns, new object[] { "Samurai", 1982 });
+            build.InsertData(TABLE, columns, new object[] { "Se", 1992 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder builder)
+        {
+            builder.Sql("DELETE FROM Musicas");
+        }
+    }
+}
