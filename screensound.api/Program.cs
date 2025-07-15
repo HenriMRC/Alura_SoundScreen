f6f8e4ad966310dfa32475be78503c5c45684f6d
Henrique <henrimrc@hotmail.com>
2025-07-15 12:03:17 +0100

    Delete and Put

--- screensound.api/Program.cs
+++ screensound.api/Program.cs
@@ -7,7 +7,6 @@ using screensound.core.data;
 using screensound.core.data.dal;
 using screensound.core.models;
 using System.Text.Json.Serialization;
-
 using JsonOptions = Microsoft.AspNetCore.Http.Json.JsonOptions;
 
 namespace screensound.api;
@@ -17,13 +16,19 @@ public class Program
     public static void Main(string[] args)
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
         builder.Services.Configure<JsonOptions>(ConfigureJson);
+
+        builder.Services.AddDbContext<ScreenSoundContext>();
+        builder.Services.AddTransient<DAL<Artist>>();
 
         WebApplication app = builder.Build();
 
         app.MapGet("/artists", GetArtists);
-        app.MapGet("/artists/{name}", GetArtistsByName);
+        app.MapGet(string.Format(GET_ARTIST_BY_NAME_ROUTE, "{name}"), GetArtistsByName);
         app.MapPost("/artists", PostArtist);
+        app.MapDelete("/artists/{id}", RemoveArtist);
+        app.MapPut("/artists", UpdateArtist);
 
         app.Run();
     }
@@ -34,17 +39,14 @@ public class Program
         options.SerializerOptions.WriteIndented = true;
     }
 
-    private static IResult GetArtists()
+    private static IResult GetArtists([FromServices] DAL<Artist> dal)
     {
-        ScreenSoundContext context = new();
-        DAL<Artist> dal = new(context);
         return Results.Ok(dal.GetList());
     }
 
-    private static IResult GetArtistsByName(string name)
+    private const string GET_ARTIST_BY_NAME_ROUTE = "/artists/{0}";
+    private static IResult GetArtistsByName([FromServices] DAL<Artist> dal, string name)
     {
-        ScreenSoundContext context = new();
-        DAL<Artist> dal = new(context);
         Artist? data = dal.First(a => name.Equals(a.Name, System.StringComparison.CurrentCultureIgnoreCase));
         if (data is null)
             return Results.NotFound();
@@ -52,12 +54,33 @@ public class Program
             return Results.Ok(data);
     }
 
-    private static IResult PostArtist([FromBody] Artist artist)
+    private static IResult PostArtist([FromServices] DAL<Artist> dal, [FromBody] Artist artist)
     {
-        ScreenSoundContext context = new();
-        DAL<Artist> dal = new(context);
         EntityEntry<Artist> entity = dal.Add(artist);
-        return Results.Ok(entity.Entity);
+        return Results.Created(string.Format(GET_ARTIST_BY_NAME_ROUTE, artist.Name), entity.Entity);
     }
 
+    private static IResult RemoveArtist([FromServices] DAL<Artist> dal, int id)
+    {
+        Artist? artist = dal.First(dal => dal.Id == id);
+        if (artist is null)
+            return Results.NotFound();
+
+        dal.Remove(artist);
+        return Results.NoContent();
+    }
+    private static IResult UpdateArtist([FromServices] DAL<Artist> dal, [FromBody] Artist artist)
+    {
+        Artist? artistOnDb = dal.First(a => a.Id == artist.Id);
+
+        if (artistOnDb is null)
+            return Results.NotFound();
+
+        artistOnDb.Name = artist.Name;
+        artistOnDb.Bio = artist.Bio;
+        artistOnDb.ProfileImage = artist.ProfileImage;
+
+        EntityEntry<Artist> result = dal.Update(artistOnDb);
+        return Results.Ok(result.Entity);
+    }
 }
