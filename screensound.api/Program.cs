5ff903e86afa6a1df2fc21d386e166f9195d4397
Henrique <henrimrc@hotmail.com>
2025-07-17 11:16:05 +0100

    Added routes for musics

--- screensound.api/Program.cs
+++ screensound.api/Program.cs
@@ -24,6 +24,7 @@ public class Program
 
         builder.Services.AddDbContext<ScreenSoundContext>();
         builder.Services.AddTransient<DAL<Artist>>();
+        builder.Services.AddTransient<DAL<Music>>();
 
         WebApplication app = builder.Build();
 
@@ -32,6 +33,13 @@ public class Program
         app.MapPost("/artists", PostArtist);
         app.MapDelete("/artists/{id}", RemoveArtist);
         app.MapPut("/artists", UpdateArtist);
+
+        app.MapGet("/musics", GetMusics);
+        app.MapGet(string.Format(GET_MUSICS_BY_NAME_ROUTE, "{name}"), GetMusicsByName);
+        app.MapPost("/musics", PostMusic);
+        app.MapDelete("/musics/{id}", RemoveMusic);
+        app.MapPut("/musics", UpdateMusic);
+
 
         app.Run();
     }
@@ -42,6 +50,7 @@ public class Program
         options.SerializerOptions.WriteIndented = true;
     }
 
+    #region Artists
     private static async Task<IResult> GetArtists([FromServices] DAL<Artist> dal)
     {
         List<Artist> result = await dal.GetListAsync();
@@ -93,4 +102,74 @@ public class Program
         EntityEntry<Artist> result = await dal.UpdateAsync(artistOnDb);
         return Results.Ok(result.Entity);
     }
+    #endregion
+
+    #region Artists
+    private static async Task<IResult> GetMusics([FromServices] DAL<Music> dal)
+    {
+        List<Music> result = await dal.GetListAsync();
+        return Results.Ok(result);
+    }
+
+    private const string GET_MUSICS_BY_NAME_ROUTE = "/musics/{0}";
+    private static async Task<IResult> GetMusicsByName([FromServices] DAL<Music> dal, string name)
+    {
+        List<Music> data = await dal.WhereAsync(Predicate);
+        bool Predicate(Music music)
+        {
+            return name.Equals(music.Name, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        if (data is null)
+            return Results.NotFound();
+        else
+            return Results.Ok(data);
+    }
+    private static async Task<IResult> PostMusic(
+        [FromServices] DAL<Music> mdal,
+        [FromServices] DAL<Artist> adal,
+        [FromBody] Music music)
+    {
+        Artist? artist = music.Artist;
+        music.Artist = null;
+
+        EntityEntry<Music> entity = await mdal.AddAsync(music);
+
+        if (artist != null && artist.Id > 0)
+        {
+            artist = await adal.FirstAsync(a => a.Id == artist.Id);
+            if (artist != null)
+            {
+                artist.AddMusic(entity.Entity);
+                await adal.UpdateAsync(artist);
+            }
+        }
+
+        return Results.Created(string.Format(GET_ARTIST_ROUTE, music.Name), entity.Entity);
+    }
+
+    private static async Task<IResult> RemoveMusic([FromServices] DAL<Music> dal, int id)
+    {
+        Music? artist = await dal.FirstAsync(dal => dal.Id == id);
+        if (artist is null)
+            return Results.NotFound();
+
+        await dal.RemoveAsync(artist);
+        return Results.NoContent();
+    }
+
+    private static async Task<IResult> UpdateMusic([FromServices] DAL<Music> dal, [FromBody] Music music)
+    {
+        Music? artistOnDb = await dal.FirstAsync(m => m.Id == music.Id);
+
+        if (artistOnDb is null)
+            return Results.NotFound();
+
+        artistOnDb.Name = music.Name;
+        artistOnDb.YearOfRelease = music.YearOfRelease;
+
+        EntityEntry<Music> result = await dal.UpdateAsync(artistOnDb);
+        return Results.Ok(result.Entity);
+    }
+    #endregion
 }
