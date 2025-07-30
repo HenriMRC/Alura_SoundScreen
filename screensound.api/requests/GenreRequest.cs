13664380a63f751062b19ff24bf8be9ae93a16bd
Henrique <henrimrc@hotmail.com>
2025-07-30 11:21:55 +0100

    Added GenreRequest

---
+++ screensound.api/requests/GenreRequest.cs
@@ -0,0 +1,5 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace screensound.api.requests;
+
+public record GenreRequest([Required] string Name, string? Description);
--- screensound.api/requests/MusicRequest.cs
+++ screensound.api/requests/MusicRequest.cs
@@ -1,16 +1,21 @@
 ﻿using screensound.core.models;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace screensound.api.requests;
 
-public record MusicRequest([Required] string Name, [Required] int ArtistId, int? YearOfRelease)
+public record MusicRequest([Required] string Name, [Required] int ArtistId,
+    int? YearOfRelease, GenreRequest[]? genres)
 {
-    public static implicit operator Music(MusicRequest artist)
+    public static implicit operator Music(MusicRequest music)
     {
-        (string name, _, int? yearOfRelease) = artist;
+        (string name, _, int? yearOfRelease, GenreRequest[]? genres) = music;
         Music output = new(name);
         if (yearOfRelease.HasValue)
             output.YearOfRelease = yearOfRelease.Value;
+        output.Genres = genres?.Select(g => new Genre(g.Name) { Description = g.Description }).ToList()
+                            ?? new List<Genre>();
 
         return output;
     }
